308a87c02a5f60457bb858027bf8a70428f0b5ea
EduardoSanjines <eduardosanjines@gmail.com>
2018-07-06 19:29:37 +0200

    Fix UnitTest Close #3

--- PooVuelingXunit.Nmock3.Unit.Tests/CalculadoraFixture.cs
+++ PooVuelingXunit.Nmock3.Unit.Tests/CalculadoraFixture.cs
@@ -4,21 +4,22 @@ using System.Linq;
 using System.Text;
 using NMock;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PooVuelingXunit.Nmock3.Unit.Tests
 {
     public class CalculadoraFixture
     {
+   
         private MockFactory _factory = new MockFactory();
-
         public CalculadoraFixture() {
 
-            var mock = _factory.CreateMock<ICalculadora>();
+            var mock = _factory.CreateMock<Calculadora>();
             mock.Expects.One.MethodWith(_ => _.Division(4, 2)).WillReturn((2));
             mock.Expects.One.MethodWith(_ => _.Multiplicacion(2, 2)).WillReturn((4));
             mock.Expects.One.MethodWith(_ => _.Resta(4, 4)).WillReturn((0));
             mock.Expects.One.MethodWith(_ => _.Suma(2, 2)).WillReturn((4));
-
+            MockObject = mock.MockObject;
         }
         public ICalculadora MockObject { get; private set; }
     }
--- PooVuelingXunit.Nmock3.Unit.Tests/CalculadoraTests.cs
+++ PooVuelingXunit.Nmock3.Unit.Tests/CalculadoraTests.cs
@@ -1,16 +1,12 @@
-﻿using PooVuelingXunit;
-using System;
-using NMock;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PooVuelingXunit.Nmock3.Unit.Tests;
 using Xunit;
 
-namespace PooVuelingXunit.Tests
+namespace PooVuelingXunit.Nmock3.Unit.Tests
 {
-    //[TestClass()]
     public class CalculadoraTests: IClassFixture<CalculadoraFixture>
     {
         CalculadoraFixture fixture;
@@ -24,27 +20,26 @@ namespace PooVuelingXunit.Tests
         public void DivisionTest()
         {
             Assert.Equal(2, fixture.MockObject.Division(4, 2));
+
         }
 
         [Fact]
         public void MultiplicacionTest()
         {
-            Assert.Equal(4, fixture.MockObject.Multiplicacion(2, 2));
 
+            Assert.Equal(4, fixture.MockObject.Multiplicacion(2, 2));
         }
 
         [Fact]
         public void RestaTest()
         {
-            Assert.Equal(2, fixture.MockObject.Resta(4, 2));
-
+           Assert.Equal(0, fixture.MockObject.Resta(4, 4));
         }
 
         [Fact]
         public void SumaTest()
         {
-            Assert.Equal(6, fixture.MockObject.Suma(4, 2));
-
+            Assert.Equal(4, fixture.MockObject.Suma(2, 2));
         }
     }
 }
