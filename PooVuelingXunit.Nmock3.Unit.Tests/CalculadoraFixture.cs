8c109b0dbb007bbefc9de539aa0e34a2ce350268
EduardoSanjines <eduardosanjines@gmail.com>
2018-07-06 19:45:26 +0200

    Fix UnitTest Close #3

--- PooVuelingXunit.Nmock3.Unit.Tests/CalculadoraFixture.cs
+++ PooVuelingXunit.Nmock3.Unit.Tests/CalculadoraFixture.cs
@@ -14,7 +14,7 @@ namespace PooVuelingXunit.Nmock3.Unit.Tests
         private MockFactory _factory = new MockFactory();
         public CalculadoraFixture() {
 
-            var mock = _factory.CreateMock<Calculadora>();
+            var mock = _factory.CreateMock<ICalculadora>();
             mock.Expects.One.MethodWith(_ => _.Division(4, 2)).WillReturn((2));
             mock.Expects.One.MethodWith(_ => _.Multiplicacion(2, 2)).WillReturn((4));
             mock.Expects.One.MethodWith(_ => _.Resta(4, 4)).WillReturn((0));
